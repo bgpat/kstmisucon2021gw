0441d0071dc8d92b54cb34742ae609f068d72e39
bgpat <bgpat@users.noreply.github.com>
2021-05-02 01:49:21 +0900

    Up workload

--- home/ishocon/benchmark_wrap.sh
+++ home/ishocon/benchmark_wrap.sh
@@ -1,1 +1,1 @@
-./benchmark 2>&1 | grep "Score:" | sed -e 's/.*Score: \(\d*\)/\1/g' | xargs -I{} curl -X POST -d "{\"score\":{}, \"timestamp\":{ \".sv\":\"timestamp\" } }" "https://kstmisucon2021gw-default-rtdb.firebaseio.com/teams/bgpat.json"
+./benchmark --workload 10 2>&1 | grep "Score:" | sed -e 's/.*Score: \(\d*\)/\1/g' | xargs -I{} curl -X POST -d "{\"score\":{}, \"timestamp\":{ \".sv\":\"timestamp\" } }" "https://kstmisucon2021gw-default-rtdb.firebaseio.com/teams/bgpat.json"
